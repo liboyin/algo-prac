e0277386a16e06faa23e75efaa86069d3857981f
Libo Yin <liboyin830@gmail.com>
2020-04-27 23:11:08 +1000

    Added documentation and tests to some functions in lib.py.
    Improved clarity of the max-sum-subarray problem.

--- arrays/max_sum_subarray.py
+++ arrays/max_sum_subarray.py
@@ -1,30 +1,40 @@
+"""Kadane's algorithm of finding the subarray with the maximum sum
+
+https://en.wikipedia.org/wiki/Maximum_subarray_problem
+
+Observations:
+    1. A max-sum subarray must start & finish with a positive number.
+    2. It is safe to discard a subarray with a non-positive sum.
+
+Time complexity is O(n). Space complexity is O(1).
+
+Problem from Jon Bentley's Programming Pearls:
+    If all elements are real numbers uniformly distributed in range [-1, 1],
+    what is the expectation of this maximum sum as a function of size?
+
+Empirical evidence seems to suggest a log-log linear relationship:
+    size=31, Ex=3.299060207545906
+    size=100, Ex=7.011417260111707
+    size=316, Ex=12.101707427755215
+    size=1000, Ex=22.20458485964562
+    size=3162, Ex=40.07622280167929
+    size=10000, Ex=72.16213987203754
+"""
+from typing import List, Tuple
+
 from lib import argmax, stated_map
 
-def search(arr):
-    """
-    Kadane's algorithm of finding the subarray with the maximum sum.
-    Observation: 1. A max-sum subarray must start & finish with a positive number.
-        2. It is safe to discard a subarray with a non-positive sum.
-    Time complexity is O(n). Space complexity is O(1).
-    Problem from Jon Bentley's Programming Pearls: If all elements are real numbers uniformly distributed in range
-        [-1, 1], what is the expectation of this maximum sum as a function of size?
-    Empirical evidence seems to suggest a log-log linear relationship:
-        size=31, Ex=3.299060207545906
-        size=100, Ex=7.011417260111707
-        size=316, Ex=12.101707427755215
-        size=1000, Ex=22.20458485964562
-        size=3162, Ex=40.07622280167929
-        size=10000, Ex=72.16213987203754
-    :param arr: list[num]
-    :return: num
-    """
+
+def search(arr: List[int]) -> int:
     max_sum = max(stated_map(lambda x, s: x + s if x + s > 0 else 0, arr, 0))
     if max_sum > 0:
         return max_sum
     return max(arr)  # if all elements are non-positive
 
-def search2(arr):  # Kadane's algorithm with reconstruction. constant space
-    acc, start = 0, 0  # start: pointer to the starting index
+
+def search2(arr: List[int]) -> Tuple[int, int, int]:
+    """Kadane's algorithm with reconstruction."""
+    start, acc = 0, 0  # start: pointer to the starting index
     max_range = 0, 0, 0  # range_start, range_end (inclusive), range_acc
     for i, x in enumerate(arr):
         if x + acc <= 0:
@@ -40,6 +50,7 @@ def search2(arr):  # Kadane's algorithm with reconstruction. constant space
         return max_range
     i, x = argmax(arr, val=True)
     return i, i, x
+
 
 if __name__ == '__main__':
     from random import randint
--- lib.py
+++ lib.py
@@ -2,52 +2,147 @@ import random
 import re
 from itertools import product, tee, zip_longest
 from math import floor, log2
+from typing import (Any, Callable, Dict, Generator, Iterable, Iterator, List,
+                    Optional, Sequence, Tuple, TypeVar, Union)
 
-def identity(x):
+T = TypeVar('T')
+V = TypeVar('V')
+
+
+def identity(x: T) -> T:
+    """The identity function."""
     return x
 
-def argmax(iterable, key=identity, val=False):  # in accordance with max(), if multiple max exist, returns the first
-    r = max(enumerate(iterable), key=lambda x: key(x[1]))
+
+def argmax(
+    iterable: Iterable[T],
+    key: Callable[[T], Any] = identity,
+    val: bool = False
+) -> Union[int, Tuple[int, T]]:
+    """Find the first index `i` where `key(iterable[i])` is the maximum.
+
+    Also see `argmin()`.
+
+    Args:
+        iterable (Iterable[T]): items to iterate
+        key (Callable[[T], Any], optional): function of one element to extract key for each element
+        val (bool, optional): if False, returns index only; if True, returns index and max value
+    """
+    r: Tuple[int, T] = max(enumerate(iterable), key=lambda x: key(x[1]))
     return r if val else r[0]
 
-def argmin(iterable, key=identity, val=False):
-    r = min(enumerate(iterable), key=lambda x: key(x[1]))
+
+def argmin(iterable: Iterable[T], key: Callable = identity, val: bool = False) -> Union[int, Tuple[int, T]]:
+    """Find the first index `i` where `key(iterable[i])` is the minimum.
+
+    Also see `argmin()`.
+
+    Args:
+        iterable (Iterable[T]): items to iterate
+        key (Callable[[T], Any], optional): function of one element to extract key for each element
+        val (bool, optional): if False, returns index only; if True, returns index and min value
+    """
+    r: Tuple[int, T] = min(enumerate(iterable), key=lambda x: key(x[1]))
     return r if val else r[0]
 
-def argmax_2d(mat, val=False):
+
+def argmax_2d(mat: Sequence[Sequence[T]], val: bool = False) -> Union[Tuple[int, int], Tuple[int, int, T]]:
+    """Find the first index pair ``(i, j)`` s.t. ``mat[i][j]`` is the maximum.
+
+    Args:
+        mat: must be indexed
+        val: if False, returns index pair ``(i, j)`` only; if True, returns ``(i, j, mat[i][j])``
+
+    Returns:
+        see ``val``
+    """
+    # TODO: make iterator-based s.t. runs on non-regular form
     m, n = len(mat), len(mat[0])
-    i, j = max(product(range(m), range(n)), key=lambda x: mat[x[0]][x[1]])  # lambda cannot unpack tuple in Python 3
+    i, j = max(product(range(m), range(n)), key=lambda x: mat[x[0]][x[1]])
     return (i, j, mat[i][j]) if val else (i, j)
 
-def batch_replace(text, rep):  # rep: dict[str, str]. from_test -> to_text
-    rx = re.compile('|'.join(map(re.escape, rep.keys())))
-    return rx.sub(lambda x: rep[x.group(0)], text)  # re.sub accepts function as pattern
 
-def bin_search_left(arr, x, left=0, right=None, key=identity):  # identical to bisect_left
-    right = len(arr) - 1 if right is None else right - 1
+def batch_replace(text: str, rep: Dict[str, str]) -> str:
+    """
+    Batch version of ``re.sub()``.
+
+    Args:
+        text: input text
+        rep: dictionary of replacement
+
+    Returns:
+        replaced text
+    """
+    rx = re.compile('|'.join(map(re.escape, rep.keys())))
+    # re.sub accepts function as pattern
+    return rx.sub(lambda x: rep[x.group(0)], text)
+
+
+def bin_search_left(arr: Sequence[T], x: T, left: int = 0, right: Optional[int] = None,
+                    key: Callable[[T], Any] = identity) -> int:
+    """Generalised re-implementation of ``bisect.bisect_left()``.
+
+    Args:
+        arr: sorted list to search in
+        x: item to insert
+        left: left boundary of search (inclusive)
+        right: right boundary of search (exclusive)
+        key: mapping function s.t. search is equivalently performed on ``map(key, arr)``
+
+    Returns:
+        left-most index to insert ``x`` such that ``arr`` remains sorted after insertion
+    """
+    right = len(arr) - 1 if right is None else right - 1  # right is inclusive after this line
     while left <= right:
         mid = (left + right) // 2
-        # side note: left + right may overflow in other languages. in such case, use left + (right - left) // 2 instead
+        # side note: left + right may overflow in some other languages. use left + (right - left) // 2 instead
         if x <= key(arr[mid]):
             right = mid - 1
         else:
             left = mid + 1
     return left
 
-def bin_search_left2(arr, x, left=0, right=None, key=identity):
-    # this algorithm appeared in Jon Bentley's Programming Pearls sec 9.3. in this implementation, since step size
-    # generation is delegated to yield_while, the empirical speed is about half of bin_search_left
+
+def bin_search_left2(arr: Sequence[T], x: T, left: int = 0, right: Optional[int] = None,
+                     key: Callable = identity) -> int:
+    """This algorithm appeared in Jon Bentley's Programming Pearls, sec 9.3. In this implementation, since step size
+    generation is delegated to ``yield_while``, the empirical speed is about half of ``bin_search_left()``.
+
+    Args:
+        arr: sorted list to search in
+        x: item to insert
+        left: left boundary of search (inclusive)
+        right: right boundary of search (exclusive)
+        key: mapping function s.t. search is equivalently performed on ``map(key, arr)``
+
+    Returns:
+        left-most index to insert ``x`` such that ``arr`` remains sorted after insertion
+    """
     left -= 1  # left: off the left of the search range
     if right is None:
         right = len(arr)  # right: off the right of the search range
-    for step in yield_while(2 ** floor(log2(right-left)), lambda x: x > 0, lambda x: x >> 1):
+    for step in yield_while(2 ** floor(log2(right - left)), lambda x: x > 0, lambda x: x >> 1):
         # [2 ^ floor(log_2(n)), ..., 4, 2, 1]. may include n, but not 0
         if left + step < right and key(arr[left + step]) < x:
             left += step
     return left + 1
 
-def bin_search_right(arr, x, left=0, right=None, key=identity):  # identical to bisect_right
-    right = len(arr) - 1 if right is None else right - 1
+
+def bin_search_right(arr: Sequence[T], x: T, left: int = 0, right: Optional[int] = None,
+                     key: Callable = identity) -> int:
+    """Generalised re-implementation of ``bisect.bisect_right()``.
+
+    Args:
+        arr: sorted list to search in
+        x: item to insert
+        left: left boundary of search (inclusive)
+        right: right boundary of search (exclusive)
+        key: mapping function s.t. search is equivalently performed on ``map(key, arr)``
+
+    Returns:
+        right-most index to insert ``x`` such that ``arr`` remains sorted after insertion
+    """
+    right = len(arr) - 1 if right is None else right - 1  # right is inclusive after this line
     while left <= right:
         mid = (left + right) // 2
         if x < key(arr[mid]):
@@ -56,7 +151,10 @@ def bin_search_right(arr, x, left=0, right=None, key=identity):  # identical to 
             left = mid + 1
     return left
 
-def fails_as(exception, func, *args, **kwargs):  # returns whether a function throws a specified exception
+
+def fails_as(exception, func, *args, **kwargs):
+    # returns whether a function throws a specified exception
+    # TODO: replace with unittest.AssertRaises()
     try:
         func(*args, **kwargs)
     except Exception as ex:
@@ -64,13 +162,20 @@ def fails_as(exception, func, *args, **kwargs):  # returns whether a function th
     else:  # if no exception is thrown
         return False
 
-def filter2(func, iterable):
-    rs = [[], []]  # [negatives, positives]
+
+def filter2(func: Callable[[T], bool], iterable: Iterable[T]) -> Tuple[List[T], List[T]]:
+    """Re-implementation of ``filter()`` that returns both positive and negative results.
+
+    Returns:
+        list of negative, list of positives
+    """
+    rs: Tuple[List[T], List[T]] = ([], [])  # (negatives, positives)
     for x in iterable:
         rs[int(func(x))].append(x)
     return rs
 
-def filter3(iterable, pivot, key=identity):
+
+def filter3(iterable: Iterable[T], pivot: T, key=identity):
     lt, eq, gt = [], [], []
     for x in iterable:
         k = key(x)
@@ -81,6 +186,7 @@ def filter3(iterable, pivot, key=identity):
         else:
             eq.append(x)
     return lt, eq, gt
+
 
 def filter3_inplace(arr, pivot, key=identity):
     i, j, k = 0, 0, len(arr)  # key(arr[:i]) < pivot; key(arr[i:j]) == pivot; key(arr[k:]) > pivot
@@ -93,37 +199,57 @@ def filter3_inplace(arr, pivot, key=identity):
         elif x == pivot:
             j += 1
         else:
-            arr[j], arr[k-1] = arr[k-1], arr[j]  # swap x with the rightmost unprocessed
+            arr[j], arr[k - 1] = arr[k - 1], arr[j]  # swap x with the rightmost unprocessed
             k -= 1
     # assert j == k
     return i, j  # note that the array rearrangement is not stable
+
 
 def filter_index(func, iterable, start=0):
     for i, x in enumerate(iterable):
         if func(x):
             yield i + start
 
-def fst(x):
-    return x[0]
 
-def is_sorted(arr, key=identity):
-    return all(key(x) <= key(y) for x, y in sliding_window(arr, 2))
+def fst(xs: Sequence[T]) -> T:
+    """Return the first element in a sequence."""
+    assert len(xs) > 0
+    return xs[0]
 
-def iter_equals(xs, ys):
+
+def is_sorted(iterable: Iterable[T], key: Callable[[T], Any] = identity) -> bool:
+    """Return whether an iterable is sorted. Consumes the iterable.
+
+    Args:
+        iterable (Iterable[T]): [description]
+        key (Callable[[T], Any], optional): [description]. Defaults to identity.
+    """
+    return all(key(x) <= key(y) for x, y in sliding_window(iterable, 2))
+
+
+def iter_equals(xs: Iterable[T], ys: Iterable[T]) -> bool:
+    """Return whether two iterables contain the same elements. Consumes both iterables.
+
+    Two iterables are considered different if they have different length.
+
+    Examples:
+        >>> iter_equals(iter([1, 2, 3]), iter([1, 2, 3]))
+        True
+        >>> iter_equals(iter([1, 2, 3]), iter([2, 3, 1]))
+        False
+        >>> iter_equals(iter([1, 2, 3]), iter([1, 2]))
+        False
+        >>> iter_equals(iter([]), iter([]))
+        True
+    """
     sentinel = object()
     for x, y in zip_longest(xs, ys, fillvalue=sentinel):
         if x is sentinel or y is sentinel or x != y:
             return False
     return True
 
+
 def kth_of_iter(iterable, k=0, default=None):
-    """
-    Returns the kth item in an iterable, or the last element if k == 0. If k > len(iterable), returns the default value.
-    :param iterable: iterable[T]
-    :param k: int. cardinal number unless k == 0
-    :param default: D
-    :return: Union[T,D]
-    """
     assert k >= 0
     k -= 1  # to ordinal number
     i, x = -1, default
@@ -132,18 +258,20 @@ def kth_of_iter(iterable, k=0, default=None):
             return x
     return x  # covers k == -1 or k > len(iterable) or empty iterator
 
-def np_index(*idx):  # NumPy style indexing
+
+def np_index(*idx: int):  # NumPy style indexing for Python objects
     def index(x):
         for i in idx:
             x = x[i]
         return x
     return index
 
+
 def randints(lower, upper, n):  # returns a sorted list of n unique random integers within a given range
     m = upper - lower + 1
     assert 0 <= n <= m
     r = []
-    for x in range(lower, upper+1):
+    for x in range(lower, upper + 1):
         if n == 0:
             break
         assert m >= 1  # if the loop finished by itself, m == 0 afterwards
@@ -155,49 +283,182 @@ def randints(lower, upper, n):  # returns a sorted list of n unique random integ
         m -= 1
     return r
 
+
 def rank(arr, distinct=True):
-    a = sorted(enumerate(arr), key=np_index(1))  # (idx, val), sorted by val
+    a = sorted(enumerate(arr), key=snd)  # (idx, val), sorted by val
     if distinct:
         a = sorted(enumerate(a), key=np_index(1, 0))  # (rank, (idx, val)), sorted by idx
     else:
         a = list(enumerate(a))  # (rank, (idx, val)), sorted by val
         for i, x in enumerate(a[1:], start=1):  # skip a[0]
-            if x[1][1] == a[i-1][1][1]:  # same val
-                a[i] = a[i-1][0], x[1]  # tuple is immutable
+            if x[1][1] == a[i - 1][1][1]:  # same val
+                a[i] = a[i - 1][0], x[1]  # tuple is immutable
         a = sorted(a, key=np_index(1, 0))  # (rank, (idx, val)), sorted by idx
     return [x[0] for x in a]
 
-def rev_enumerate(iterable):
-    return reversed(list(enumerate(iterable)))
 
-def rev_range(fst, snd=None):
-    if snd is None:
-        return reversed(range(fst))
-    return reversed(range(fst, snd))
+def rev_range(
+    fst: int,
+    snd: Optional[int] = None
+) -> Iterator[int]:
+    """Reversed version of built-in `range()`, without the step argument.
 
-def safe_query(arr, i, default):
-    if i < 0 or i >= len(arr):
+    Examples:
+        >>> list(rev_range(5))
+        [4, 3, 2, 1, 0]
+        >>> list(rev_range(1, 5))
+        [4, 3, 2, 1]
+        >>> list(rev_range(-1))
+        []
+    """
+    return reversed(range(fst) if snd is None else range(fst, snd))
+
+
+def rev_enumerate(seq: Sequence[T]) -> Iterator[Tuple[int, T]]:
+    """Reversed variant of built-in `enumerate()`, without the start argument.
+
+    Note that the first argument of `enumerate()` has type `Iterable`, but the equivalent argument here must have length.
+
+    Examples:
+        >>> list(rev_enumerate('abc'))
+        [(2, 'c'), (1, 'b'), (0, 'a')]
+    """
+    yield from zip(reversed(range(len(seq))), reversed(seq))
+
+
+def safe_query(
+    seq: Sequence[T],
+    i: int,
+    default: T,
+    allow_neg: bool = False,
+) -> T:
+    """Query a sequence. In case of an IndexError, return a default value.
+
+    Whether negative index is allowed is controlled by `allow_neg`.
+
+    Examples:
+        >>> safe_query([1, 2, 3], 1, 0)
+        2
+        >>> safe_query([1, 2, 3], 3, 0)
+        0
+        >>> safe_query([1, 2, 3], -1, 0)
+        0
+        >>> safe_query([1, 2, 3], -1, 0, allow_neg=True)
+        3
+
+    Args:
+        seq (Sequence[T]): queried sequence
+        i (int): queried index
+        default (T): default value
+        allow_neg (bool, optional): if False, return `default` if `i` is negative
+    """
+    if not allow_neg and i < 0:
         return default
-    return arr[i]
+    try:
+        return seq[i]
+    except IndexError:
+        return default
 
-def snd(x):
-    return x[1]
 
-def stated_map(func, iterable, state, prefix=False):
+def snd(xs: Sequence[T]) -> T:
+    """Return the second element in a sequence.
+
+    Args:
+        xs (Sequence[T]): queried sequence
+    """
+    assert len(xs) > 1, len(xs)
+    return xs[1]
+
+
+def stated_map(
+    func: Callable[[T, V], T],
+    iterable: Iterable[V],
+    state: T,
+    prefix: bool = True,
+) -> Generator[T, None, None]:
+    """A stated variant of `map()`.
+
+    Whether the initial state is yielded is controlled by `prefix`.
+
+    Also see `yield_while()`.
+
+    Examples:
+        >>> list(stated_map(lambda x, y: x * y, [2, 3, 4], 1))
+        [1, 2, 6, 24]
+        >>> list(stated_map(lambda x, y: x * y, [2, 3, 4], 1, prefix=False))
+        [2, 6, 24]
+
+    Args:
+        func (Callable[[T, V], T]): a function that takes in the current state 
+            and an element from the iterable, and generates the next state
+        iterable (Iterable[V]): items to iterate
+        state (T): initial state
+        prefix (bool, optional): if True, yield the initial state immediately
+    """
     if prefix:
         yield state
     for x in iterable:
-        state = func(x, state)
+        state = func(state, x)
         yield state
 
-def sliding_window(iterable, size):
-    iters = tee(iterable, size)
+
+def sliding_window(
+    iterable: Iterable[T],
+    size: int,
+) -> Iterator[Tuple[T, ...]]:
+    """Create a sliding window with specified size on a given iterable object.
+
+    Examples:
+        >>> list(sliding_window(range(5), size=3))
+        [(0, 1, 2), (1, 2, 3), (2, 3, 4)]
+        >>> list(sliding_window(range(5), size=0))
+        []
+
+    Args:
+        iterable (Iterable[T]): items to iterate
+        size (int): size of the sliding window
+    """
+    iterators: Tuple[Iterator[T], ...] = tee(iterable, size)
     for i in range(size):
         for _ in range(i):
-            next(iters[i], None)
-    return zip(*iters)
+            next(iterators[i], None)
+    return zip(*iterators)
 
-def yield_while(state, term, trans):
-    while term(state):
-        yield state
-        state = trans(state)
+
+def yield_while(
+    state: T,
+    term_func: Callable[[T], bool],
+    trans_func: Callable[[T], T],
+    prefix: bool = True,
+) -> Generator[T, None, None]:
+    """Repeatedly apply a transition function, yield the result, until a terminating condition is met.
+
+    Whether the initial state is yielded is controlled by `prefix`.
+
+    Also see `stated_map()`.
+
+    Examples:
+        >>> list(yield_while(1, lambda x: x < 10, lambda x: x * 2))
+        [1, 2, 4, 8]
+        >>> list(yield_while(1, lambda x: x < 10, lambda x: x * 2, prefix=False))
+        [2, 4, 8]
+        >>> list(yield_while(10, lambda x: x < 10, lambda x: x * 2))
+        []
+
+    Args:
+        state (T): initial state
+        term_func (Callable[[T], bool]): terminating function
+        trans_func (Callable[[T], T]): transition function
+        prefix (bool, optional): if True, yield the initial state immediately,
+            unless it meets the terminating condition
+    """
+    while term_func(state):
+        if prefix:
+            yield state
+        prefix = True
+        state = trans_func(state)
+
+
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod(verbose=True)
